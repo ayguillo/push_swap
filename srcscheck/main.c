2bfaa9d48e127055df332684730ad92d0deef038
Aymeric GUILLOCHET <ayguillo@e2r10p19.42.fr>
2019-02-12 14:43:09 +0100

    norme check

--- srcscheck/main.c
+++ srcscheck/main.c
@@ -6,7 +6,7 @@
 /*   By: ayguillo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/18 14:10:22 by ayguillo          #+#    #+#             */
-/*   Updated: 2019/02/11 17:04:10 by ayguillo         ###   ########.fr       */
+/*   Updated: 2019/02/12 14:42:33 by ayguillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,26 @@ void		ft_delchecklist(t_check **list)
 	}
 }
 
+static void	ft_options(char **av, int *j, int *n)
+{
+	if (!(ft_strcmp(av[1], "-v")))
+	{
+		*j = -1;
+		*n = 1;
+	}
+}
+
+static void	ft_end(int j, t_pslist **lista, t_pslist **listb, char **line)
+{
+	if (j > 0)
+	{
+		ft_freelist(lista, listb);
+		ft_strdel(line);
+		return ;
+	}
+	ft_verif(*lista) == 1 ? ft_putstr("OK\n") : ft_putstr("KO\n");
+}
+
 int			main(int ac, char **av)
 {
 	int			n;
@@ -46,13 +66,8 @@ int			main(int ac, char **av)
 	ft_init(&lista, &listb, &line, &j);
 	if (ac < 2)
 		return (0);
-	if (!(ft_strcmp(av[1], "-v")))
-	{
-		j = -1;
-		n = 1;
-	}
+	ft_options(av, &j, &n);
 	while (av[++n])
-	{
 		if (ft_parsing(av[n], &lista, &listb) > 0 ||
 				!(lista = ft_addr(lista, ft_atoi(av[n]))))
 		{
@@ -60,15 +75,8 @@ int			main(int ac, char **av)
 			ft_putstr("Error\n");
 			return (-1);
 		}
-	}
 	ft_checker(line, &lista, &listb, &j);
-	if (j > 0)
-	{
-		ft_freelist(&lista, &listb);
-		ft_strdel(&line);
-		return (-1);
-	}
-	ft_verif(lista) == 1 ? ft_putstr("OK\n") : ft_putstr("KO\n");
+	ft_end(j, &lista, &listb, &line);
 	ft_freelist(&lista, &listb);
 	return (0);
 }
